c0c1149e0232f78709ad05faff91245d8c9b84e9
David wanjohi <davidwagura83@gmail.com>
2024-03-26 18:37:05 +0300

    create controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function create(Request $request)
+    {
+        $product = new Product;
+        $product->name = 'Payback 2';
+        $product->price = 40;
+        $product->save();
+
+        $category = Category::find([3, 4]);
+        $product->categories()->attach($category);
+        return 'Success';
+    }
+}
